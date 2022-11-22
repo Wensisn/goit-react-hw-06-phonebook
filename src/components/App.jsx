dd26576cd7b751fd722bf50a369f5dc5b14da776
Zubenko1 <zubenkodp@gmail.com>
2022-11-22 03:23:24 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,6 @@
 import { ContactsList } from './Contacts/Contacts';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsFilter } from './StatusFilter/ContactsFilter';
-
-import { getVisibleContacts } from 'redux/selectors';
-import { useSelector } from 'react-redux';
-
 export const App = () => {
   return (
     <div>
